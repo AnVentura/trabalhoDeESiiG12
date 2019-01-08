33957660ea3a8e6424fa245da5a6f0e9b264707f
Bruno <brunocunha57@gmail.com>
2019-01-08 18:06:08 +0000

    Melhorei a REMOVER.
    Nota: Não tenho a certeza se está bem

--- ESIIPROJECT/src/main/java/Main.java
+++ ESIIPROJECT/src/main/java/Main.java
@@ -28,18 +28,23 @@ public class Main {
     }
 
     public static String Remover(String frase) {
-        //String aux;
-        //DIGITOS
-        frase = frase.replace("0","");
-        frase = frase.replace("1","");
-        frase = frase.replace("2","");
-        frase = frase.replace("3","");
-        frase = frase.replace("4","");
-        frase = frase.replace("5","");
-        frase = frase.replace("6","");
-        frase = frase.replace("7","");
-        frase = frase.replace("8","");
-        frase = frase.replace("9","");
+
+
+        File dir = new File("C:\\Users\\ANDRE\\Desktop\\TEST\\");
+
+        for (File file : dir.listFiles()) {
+            Scanner s = new Scanner(file);
+
+            while (scanner.hasNext()) {
+                frase = frase.replaceAll("\\d","");
+            })
+
+            s.close();
+        }
+
+
+
+
         //SINAIS DE PONTUAÇÃO
         frase = frase.replace(",","");
         frase = frase.replace(";","");
